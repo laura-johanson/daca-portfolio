97ac18e2bdce78c48b1194bc6f5fa0d61d6b1294
laura-johanson <laurajohanson2@gmail.com>
2026-09-07 08:26:54 +0300

    Update README

--- week-4/team/week4_team_aggregation.sql
+++ week-4/team/week4_team_aggregation.sql
@@ -1,1 +1,13 @@
-Nädal 4
+# Nädal 4: Team SQL Aggregation
+
+## Kirjeldus
+
+Meeskonnatööna koostatud analüüs UrbanStyle'i andmetest ja peamistest tähelepanekutest.
+
+## Minu roll
+
+**Roll B – Kliendigruppide analüüs (Customer Segmentation)**
+
+## Meeskonna töö
+
+Koondraport on koostatud meie meeskonna ühises GitHubi repos.
