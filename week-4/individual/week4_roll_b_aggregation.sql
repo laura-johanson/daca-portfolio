71edac2f4e377b5ac0b674169529e778282b02c7
laura-johanson <laurajohanson2@gmail.com>
2026-09-07 08:08:02 +0300

    Add query for top 10 customers by total sales
    Added a query to retrieve the top 10 customers based on total sales, considering only those with at least 2 orders.

--- week-4/individual/week4_roll_b_aggregation.sql
+++ week-4/individual/week4_roll_b_aggregation.sql
@@ -31,3 +31,23 @@ SELECT
     END AS segment
 FROM kliendi_kokkuvote
 ORDER BY kogukäive DESC;
+
+-- 2. TOP 10 klienti.
+-- Leian iga kliendi tellimuste arvu ja kogukäibe ning järjestan kliendid kogukäibe alusel.
+-- Arvesse võtan ainult kliendid, kellel on vähemalt 2 tellimust, ning valin 10 suurima kogukäibega klienti.
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    COUNT(s.sale_id) AS tellimuste_arv,
+    SUM(s.total_price) AS kogukäive
+FROM sales s
+JOIN customers c
+    ON s.customer_id = c.customer_id
+GROUP BY
+    c.customer_id,
+    c.first_name,
+    c.last_name
+HAVING COUNT(s.sale_id) >= 2
+ORDER BY kogukäive DESC
+LIMIT 10;
